eaaee1e83742a72b53d629930a440e5c6d016a1e
NguyenDacQuyet <thatao2272006@gmail.com>
2025-10-17 14:11:53 +0700

    Add files via upload

---
+++ MAD 101 AS2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int so, mu;
+    const int mod = 9901;
+    int result = 1;
+
+    cout << "So: ";
+    cin >> so;
+    cout << "Mu: ";
+    cin >> mu;
+
+    while (mu--) result = (result * so) % mod;
+
+    cout << result;
+}
